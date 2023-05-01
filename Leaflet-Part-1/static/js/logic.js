a5c8324a3401b8356edb6ec8fee2e13434f8bcdd
LailahL <lailah_libay@hotmail.com>
2023-05-01 02:11:58 -0400

    finished assignment

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -10,16 +10,26 @@ var myMap = L.map("map", {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(myMap);
 
-  function chooseCol(depth) {
-    if (depth >= 90) return "#FF0000";
-    else if (depth >= 70) return "#FF5733";
-    else if (depth >= 50) return "#FFA500";
-    else if (depth >= 30) return "#FFAE42";
-    else if (depth >= 10) return "#FFFF00";
-    else if (depth >= -10) return "#9ACD32";
-    else return "#008000";
-}
+//   function chooseCol(depth) {
+//     if (depth >= 90) return "#FF0000";
+//     else if (depth >= 70) return "#FF5733";
+//     else if (depth >= 50) return "#FFA500";
+//     else if (depth >= 30) return "#FFAE42";
+//     else if (depth >= 10) return "#FFFF00";
+//     else if (depth >= -10) return "#9ACD32";
+//     else return "#008000";
+// }
 
+function chooseCol(depth) {
+  if (depth <=-10) return "#008000";
+  else if (depth <= 10) return "#008000";
+  else if (depth <= 30) return "#9ACD32";
+  else if (depth <= 50) return "#FFFF00";
+  else if (depth <= 70) return "#FFA500";
+  else if (depth <= 90) return "#FF5733";
+  else return "#800000";
+
+}
 
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 d3.json(url).then(function(response) {
@@ -47,11 +57,12 @@ d3.json(url).then(function(response) {
     var legend = L.control({ position: "bottomright" });
     legend.onAdd = function() {
     var div = L.DomUtil.create("div", "info legend");
-    div.innerHTML += '<div><span class="dot" style="background-color: red;"></span> Depth >= 90</div>';
-    div.innerHTML += '<div><span class="dot" style="background-color: orange;"></span> 70 <= Depth < 90</div>';
-    div.innerHTML += '<div><span class="dot" style="background-color: yellow;"></span> 50 <= Depth < 70</div>';
-    div.innerHTML += '<div><span class="dot" style="background-color: yellowgreen;"></span> 30 <= Depth < 50</div>';
-    div.innerHTML += '<div><span class="dot" style="background-color: green;"></span> Depth < 30</div>';
+    div.innerHTML += '<div><span class="dot" style="background-color: #008000;"></span> -10-10</div>';
+    div.innerHTML += '<div><span class="dot" style="background-color: #9ACD32;"></span> 10-30</div>';
+    div.innerHTML += '<div><span class="dot" style="background-color: #FFFF00;"></span> 30-50</div>';
+    div.innerHTML += '<div><span class="dot" style="background-color: #FFA500;"></span> 50-70</div>';
+    div.innerHTML += '<div><span class="dot" style="background-color: #FF5733;"></span> 70-90</div>';
+    div.innerHTML += '<div><span class="dot" style="background-color: #800000;"></span> 90+</div>';
     return div;
     };
     legend.addTo(myMap);
